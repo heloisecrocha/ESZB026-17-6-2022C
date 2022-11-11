2adaffd3ba1cfbecd33e20663ef282faa3f1b7fe
Bárbara Rodrigues <60752819+bahrodrigues@users.noreply.github.com>
2022-11-11 17:42:06 -0300

    Create pwm_hw_motor.c

---
+++ lab05/pwm_hw_motor.c
@@ -0,0 +1,28 @@
+// Ajustando o PWM por HARDWARE na Raspberry Pi
+
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define pino_PWM0 18                    // o PWM sera acionado na GPIO18
+
+int main() {                            // este programa deve ser rodado com 'sudo'
+   int dc, ciclos2;
+   wiringPiSetupGpio();                 // usa a numeracao da GPIO
+   pinMode(pino_PWM0, PWM_OUTPUT);      // configura a GPIO18 com o PWM por hardware
+
+   pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
+   pwmSetRange(3495);                    // passos do duty cycle (max=4096)
+   pwmSetClock(21);                     // fornece uma frequencia de 10kHz (max=4096)
+   printf("Iniciando...\n");
+	
+   dc = 1748;
+
+     for ( ciclos2 = 0; ciclos2 < 3; ciclos2 ++) { // variando o duty cycle
+         pwmWrite ( pino_PWM0 , dc );
+         usleep (300000);
+
+     }
+         
+   printf("Fim.\n");
+   return 0;                            // a saida PWM permanece ligada apos o termino do programa
+}
