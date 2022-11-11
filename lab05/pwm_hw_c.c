e26db6d64e9d9dad710a357b2c28bf56db92fc9c
heloisecrocha <85141712+heloisecrocha@users.noreply.github.com>
2022-11-11 11:56:08 -0300

    Update pwm_hw_c.c

--- lab05/pwm_hw_c.c
+++ lab05/pwm_hw_c.c
@@ -1,8 +1,3 @@
-/** Programa baseado no codigo disponibilizado em:
-* Derek Molloy, Exploring Raspberry Pi: Interfacing to the Real World with Embedded Linux,
-* Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/
-*/
-
 // Ajustando o PWM por HARDWARE na Raspberry Pi
 
 #include <stdio.h>
@@ -15,20 +10,14 @@ int main() {                            // este programa deve ser rodado com 'su
    wiringPiSetupGpio();                 // usa a numeracao da GPIO
    pinMode(pino_PWM0, PWM_OUTPUT);      // configura a GPIO18 com o PWM por hardware
 
-   // Ajustando a frequencia do PWM em 10kHz com 128 passos de duty cycle
-   // frequencia PWM = 19,2 MHz / (divisor * range)
-   // 10000 = 19200000 / (divisor * 128) => divisor = 15
    pwmSetMode(PWM_MODE_MS);             // usando frequencia fixa
-   pwmSetRange(128);                    // passos do duty cycle (max=4096)
-   pwmSetClock(15);                     // fornece uma frequencia de 10kHz (max=4096)
+   pwmSetRange(3495);                    // passos do duty cycle (max=4096)
+   pwmSetClock(21);                     // fornece uma frequencia de 10kHz (max=4096)
    printf("Iniciando...\n");
    
-   pwmWrite(pino_PWM0, dc);
-   usleep(10000);
-     
-      
-   pwmWrite(pino_PWM0, dc);
-   usleep(10000);
+   
+   
+   
       
    printf("Fim.\n");
    return 0;                            // a saida PWM permanece ligada apos o termino do programa
